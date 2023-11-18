3a78845ed17a190874d6b87d612572c32b3ccfb7
OatmealJester <drrr032@gmail.com>
2023-11-18 18:40:03 -0500

    cleaning up dependencies

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import utils as utl
 from views import home,Misson,analysis,options,configuration
-st.set_page_config(layout="wide", page_title='Navbar sample')
+import pandas as pd
+import plotly.express as px
+st.set_page_config(layout="wide", page_title='Main Page')
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 utl.inject_custom_css()
 utl.navbar_component()
@@ -22,3 +25,4 @@ def navigation():
         home.load_view()
         
 navigation()
+
